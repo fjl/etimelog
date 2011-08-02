cbe0faeb937e899f1b8a2c9b24ec845813b80de1
Felix Lange <fjl@twurst.com>
2011-08-03 00:54:39 +0200

    read config file on startup
    write defaults if it doesn't exist

--- src/etimelog.erl
+++ src/etimelog.erl
@@ -2,8 +2,48 @@
 -behaviour(application).
 -export([start/2, stop/1]).
 
+%% --------------------------------------------------------------------------------
+%% -- application callbacks
 start(_StartType, _StartArgs) ->
-    etimelog_sup:start_link().
+    Dir        = filename:join(os:getenv("HOME"), ".etimelog"),
+    LogFile    = filename:join(Dir, "timelog.txt"),
+    ConfigFile = filename:join(Dir, "etimelogrc"),
+
+    filelib:ensure_dir(LogFile),
+
+    case filelib:is_regular(ConfigFile) of
+        true  -> read_config(ConfigFile);
+        false -> write_defaults(ConfigFile)
+    end,
+
+    etimelog_sup:start_link(LogFile).
 
 stop(_State) ->
     ok.
+
+%% --------------------------------------------------------------------------------
+%% -- helpers
+read_config(ConfigFile) ->
+    case file:consult(ConfigFile) of
+        {ok, Terms} ->
+            lists:foreach(fun ({Key, Value}) when is_atom(Key) ->
+                                  application:set_env(etimelog, Key, Value);
+                              (Term) ->
+                                  error({config_file, {illegal_term, Term}})
+                          end, Terms);
+        {error, enoent} ->
+            ok;
+        {error, Error} when is_atom(Error) ->
+            error({config_file, Error});
+        {error, {Line, Mod, Info}} ->
+            error_logger:error_msg("Error in ~/etimelog/.etimelogrc, line ~b:~s~n", [Line, Mod:format_error(Info)]),
+            error({config_file, syntax})
+    end.
+
+write_defaults(ConfigFile) ->
+    {ok, Defaults} = application:get_key(etimelog, env),
+    {ok, File}     = file:open(ConfigFile, [write, exclusive]),
+    lists:foreach(fun ({included_applications, []}) -> skip;
+                      ({Key, Value})                -> io:format(File, "{~s, ~p}.~n", [Key, Value])
+                  end, Defaults),
+    file:close(File).
--- src/etimelog_sup.erl
+++ src/etimelog_sup.erl
@@ -1,24 +1,17 @@
 -module(etimelog_sup).
 -behaviour(supervisor).
 
--export([start_link/0]).
--export([init/1]).
+-export([start_link/1, init/1]).
 
 -define(SERVER, etimlog_sup).
 
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Args, Type), {I, {I, start_link, Args}, permanent, 5000, Type, [I]}).
 
-start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, {}).
+start_link(TimelogFile) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, {TimelogFile}).
 
-init({}) ->
-    File = timelog_file(),
-    filelib:ensure_dir(File),
-
-    FileWorker = ?CHILD(etimelog_file, [File], worker),
+init({TimelogFile}) ->
+    FileWorker = ?CHILD(etimelog_file, [TimelogFile], worker),
 
     {ok, {{one_for_one, 5, 10}, [FileWorker]}}.
-
-timelog_file() ->
-    filename:join([os:getenv("HOME"), ".etimelog", "timelog.txt"]).
